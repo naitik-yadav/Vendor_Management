dd4d8dc157f7a0a1bcc8835b530fb30580c6d6d6
Naitik Singh <ntkydv16@gmail.com>
2024-05-03 00:07:36 +0530

    Update serializers

--- vendor_management_system/vendors/serializers.py
+++ vendor_management_system/vendors/serializers.py
@@ -15,3 +15,4 @@ class PurchaseOrderSerializer(serializers.ModelSerializer):
         model = PurchaseOrder
         fields = ['id', 'po_number', 'vendor', 'order_date', 'delivery_date', 'items', 'quantity', 'status',
                   'quality_rating', 'issue_date', 'acknowledgment_date']
+
